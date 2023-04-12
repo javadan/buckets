93703f340aef938cdbb7b1367b37a5f54c52de35
Daniel <daniel.brownell@gmail.com>
2023-04-12 11:26:21 +0200

    Update urls.py
    url -> re_path

--- bucketreactor/urls.py
+++ bucketreactor/urls.py
@@ -5,17 +5,17 @@ from . import views
 
 urlpatterns = (
     # Public
-    url(r'^$', views.root),
+    re_path(r'^$', views.root),
     
-    url(r'^user/auth/register/$', views.RegisterView.as_view(), name='user-register'),
-    url(r'^user/auth/login/$', views.LoginView.as_view(), name='user-login'),
-    url(r'^user/auth/logout/$', views.LogoutView.as_view(), name='user-logout'),
-    url(r'^user/auth/password/change/$', views.PasswordChangeView.as_view(), name='user-password-change'),
-    url(r'^user/auth/password/reset/$', views.PasswordResetView.as_view(), name='user-password-reset'),
+    re_path(r'^user/auth/register/$', views.RegisterView.as_view(), name='user-register'),
+    re_path(r'^user/auth/login/$', views.LoginView.as_view(), name='user-login'),
+    re_path(r'^user/auth/logout/$', views.LogoutView.as_view(), name='user-logout'),
+    re_path(r'^user/auth/password/change/$', views.PasswordChangeView.as_view(), name='user-password-change'),
+    re_path(r'^user/auth/password/reset/$', views.PasswordResetView.as_view(), name='user-password-reset'),
 
-    url(r'^user/box/$', views.BoxListCreateView.as_view(), name='user-node-view'),
-    url(r'^user/box/(?P<identifier>([a-zA-Z0-9\_\-]+))/$', views.BoxUpdateView.as_view(), name='user-node-update'),
-    url(r'^user/box/(?P<identifier>([a-zA-Z0-9\_\-]+))/measure/$', views.MeasurementListCreateView.as_view(), name='user-node-update')
+    re_path(r'^user/box/$', views.BoxListCreateView.as_view(), name='user-node-view'),
+    re_path(r'^user/box/(?P<identifier>([a-zA-Z0-9\_\-]+))/$', views.BoxUpdateView.as_view(), name='user-node-update'),
+    re_path(r'^user/box/(?P<identifier>([a-zA-Z0-9\_\-]+))/measure/$', views.MeasurementListCreateView.as_view(), name='user-node-update')
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
