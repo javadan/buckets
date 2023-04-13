259545ae6a498abe924dee18a6810852d6e3a5a7
Daniel <daniel.brownell@gmail.com>
2023-04-13 12:25:08 +0200

    Update settings.py
    middleware config changed?

--- config/settings.py
+++ config/settings.py
@@ -70,6 +70,23 @@ MIDDLEWARE_CLASSES = [
     'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'config.middleware.HealthCheckMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'config.middleware.DisableCSRF',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
 INTERNAL_IPS = ['127.0.0.1']
 
 ROOT_URLCONF = 'config.urls'
